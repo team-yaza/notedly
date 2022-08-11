cb61623dec7de731defbf0b4972bfcb3ea950bde
hyunjin <leehj0110@kakao.com>
2022-08-12 07:49:37 +0900

    feat: add reosolvers to index

--- server/src/resolvers/index.js
+++ server/src/resolvers/index.js
@@ -1,7 +1,13 @@
 const Query = require("./query")
 const Mutation = require("./mutation")
+const Note = require("./note")
+const User = require("./user")
 
 module.exports = {
   Query,
   Mutation,
+  Note,
+  User,
 }
+
+// 스키마 작성 -> 데이터 모델 작성 -> 데이터를 쿼리하거나 업데이트하는 리졸버 코드를 작성
