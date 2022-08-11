c63a0d6be76901a6db0b14ebc3f749178a93b25e
hyunjin <leehj0110@kakao.com>
2022-08-12 07:49:08 +0900

    fix: note resolver 오타

--- server/src/resolvers/note.js
+++ server/src/resolvers/note.js
@@ -1,16 +1,10 @@
-const { user } = require("./query")
-
 module.exports = {
+  // Resolve the author info for a note when requested
   author: async (note, args, { models }) => {
     return await models.User.findById(note.author)
   },
-  favoriteBy: async (note, args, { models }) => {
-    return await models.Note.find({ author: user._id }).sort({ _id: -1 })
-  },
-  notes: async (user, args, { models }) => {
-    return await models.Note.find({ author: user._id }).sort({ _id: -1 })
-  },
-  favorites: async (user, args, { models }) => {
-    return await models.Note.find({ favoritedBy: user._id }).sort({ _id: -1 })
+  // Resolved the favoritedBy info for a note when requested
+  favoritedBy: async (note, args, { models }) => {
+    return await models.User.find({ _id: { $in: note.favoritedBy } })
   },
 }
