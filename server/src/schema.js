6dda34705325241ea389747bc8c1503f0c5ef9d9
hyunjin <leehj0110@kakao.com>
2022-08-12 07:49:53 +0900

    feat: add Types to graphql schema

--- server/src/schema.js
+++ server/src/schema.js
@@ -6,35 +6,39 @@ module.exports = gql`
   type Note {
     id: ID!
     content: String!
-    author: String!
+    author: User!
+    favoriteCount: Int!
+    favoritedBy: [User]
     createdAt: DateTime!
     updatedAt: DateTime!
-    favoriteCount: Int!
-    favoritedBy: [User!]
   }
-
   type User {
     id: ID!
     username: String!
     email: String!
     avatar: String
     notes: [Note!]!
+    favorites: [Note!]!
   }
-
+  type NoteFeed {
+    notes: [Note]!
+    cursor: String!
+    hasNextPage: Boolean!
+  }
   type Query {
     notes: [Note!]!
-    note(id: ID!): Note
+    note(id: ID): Note!
     user(username: String!): User
     users: [User!]!
     me: User!
+    noteFeed(cursor: String): NoteFeed
   }
-
   type Mutation {
     newNote(content: String!): Note
     updateNote(id: ID!, content: String!): Note!
     deleteNote(id: ID!): Boolean!
+    toggleFavorite(id: ID!): Note!
     signUp(username: String!, email: String!, password: String!): String!
     signIn(username: String, email: String, password: String!): String!
-    toggleFavorite(id: ID!): Note!
   }
 `
