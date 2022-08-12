6d234f92d454a4bc2ac2a7f74ccfbb9306866b79
hyunjin <leehj0110@kakao.com>
2022-08-12 13:18:53 +0900

    feat: add globalstyle

--- client/pages/_app.js
+++ client/pages/_app.js
@@ -1,11 +1,15 @@
 import Layout from "../components/Layout"
 import "../styles/globals.css"
+import GlobalStyle from "../styles/globalStyle"
 
 function MyApp({ Component, pageProps }) {
   return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
+    <>
+      <GlobalStyle />
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </>
   )
 }
 
--- client/pages/index.jsx
+++ client/pages/index.jsx
@@ -1,6 +1,7 @@
 import Head from "next/head"
 import Image from "next/image"
 import Link from "next/link"
+import Button from "../components/Button"
 import Header from "../components/Header"
 import Navigation from "../components/Navigation"
 import Sparkle from "../components/Sparkle"
@@ -9,6 +10,7 @@ export default function Home() {
   return (
     <div>
       <p>hhi</p>
+      <Button />
     </div>
   )
 }
